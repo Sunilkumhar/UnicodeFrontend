1bf1d3a98b269fb19f9857540c03809244cb3c84
Sunilkumhar <54788016+Sunilkumhar@users.noreply.github.com>
2020-08-11 10:46:21 +0530

    added sort in table

--- Ftask 3 other api/js/sort.js
+++ Ftask 3 other api/js/sort.js
@@ -1,12 +1,11 @@
-
-function namesort(xyz){
-    console.log(xyz);
+var sum1=0,sum2=0,sum3=0;sum4=0;
+function namesort(){
+    sum1=sum1+1;
     fetch("https://corona.lmao.ninja/v2/countries", {
     "method": "GET",
     })
     .then(response => response.json()) 
         .then(data => {    
-            console.log(data)
             sorting(data);
         })
         .catch(err => {
@@ -14,10 +13,16 @@ function namesort(xyz){
         }); 
 
     function sorting (data1) {
-        console.log(xyz);
-        data1.sort(function(a, b) {
-            return b.name - a.name;
-        });
+
+        if(sum1 % 2){
+            data1.sort(function(a, b) {
+                return b.name - a.name;
+            }); 
+        }else{
+            data1.sort(function(a, b) {
+                return a.name - b.name;
+            });
+        }
         
         var x = document.getElementById("stats");
         x.innerHTML = ``;
@@ -39,14 +44,14 @@ function namesort(xyz){
         });
     }
 }
-function casessort(xyz){
-    console.log(xyz);
+function casessort(){
+    sum2=sum2+1;
+
     fetch("https://corona.lmao.ninja/v2/countries", {
     "method": "GET",
     })
     .then(response => response.json()) 
         .then(data => {    
-            console.log(data)
             sorting(data);
         })
         .catch(err => {
@@ -54,10 +59,59 @@ function casessort(xyz){
         }); 
 
     function sorting (data1) {
-        console.log(xyz);
-        data1.sort(function(a, b) {
-            return b.cases - a.cases;
+        
+        if(sum2 % 2){
+            data1.sort(function(a, b) {
+                return b.cases - a.cases;
+            }); 
+        }else{
+            data1.sort(function(a, b) {
+                return a.cases - b.cases;
+            });
+        }
+        var x = document.getElementById("stats");
+        x.innerHTML = ``;
+        data1.forEach(countrystat => {
+
+            var row = x.insertRow(-1);
+            var cell_flag = row.insertCell(0);
+            var cell_name = row.insertCell(1);
+            var cell_total = row.insertCell(2);
+            var cell_active = row.insertCell(3);
+            var cell_deaths = row.insertCell(4);
+    
+            cell_flag.innerHTML = `<img class="flag-size" src="${countrystat.countryInfo.flag}">`;
+            cell_name.innerHTML = countrystat.country;
+            cell_total.innerHTML = countrystat.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            cell_active.innerHTML = countrystat.active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            cell_deaths.innerHTML = countrystat.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    
         });
+    }
+}
+function activesort(){
+    sum3=sum3+1;
+    fetch("https://corona.lmao.ninja/v2/countries", {
+    "method": "GET",
+    })
+    .then(response => response.json()) 
+        .then(data => {    
+            sorting(data);
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
+
+    function sorting (data1) {
+        if(sum3 % 2){
+            data1.sort(function(a, b) {
+                return b.active - a.active;
+            }); 
+        }else{
+            data1.sort(function(a, b) {
+                return a.active - b.active;
+            });
+        }
         
         var x = document.getElementById("stats");
         x.innerHTML = ``;
@@ -79,14 +133,13 @@ function casessort(xyz){
         });
     }
 }
-function activesort(xyz){
-    console.log(xyz);
+function deathsort(){
+    sum4=sum4+1;
     fetch("https://corona.lmao.ninja/v2/countries", {
     "method": "GET",
     })
     .then(response => response.json()) 
         .then(data => {    
-            console.log(data)
             sorting(data);
         })
         .catch(err => {
@@ -94,50 +147,15 @@ function activesort(xyz){
         }); 
 
     function sorting (data1) {
-        console.log(xyz);
-        data1.sort(function(a, b) {
-            return b.active - a.active;
-        });
-        
-        var x = document.getElementById("stats");
-        x.innerHTML = ``;
-        data1.forEach(countrystat => {
-
-            var row = x.insertRow(-1);
-            var cell_flag = row.insertCell(0);
-            var cell_name = row.insertCell(1);
-            var cell_total = row.insertCell(2);
-            var cell_active = row.insertCell(3);
-            var cell_deaths = row.insertCell(4);
-    
-            cell_flag.innerHTML = `<img class="flag-size" src="${countrystat.countryInfo.flag}">`;
-            cell_name.innerHTML = countrystat.country;
-            cell_total.innerHTML = countrystat.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            cell_active.innerHTML = countrystat.active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            cell_deaths.innerHTML = countrystat.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    
-        });
-    }
-}
-function deathsort(xyz){
-    console.log(xyz);
-    fetch("https://corona.lmao.ninja/v2/countries", {
-    "method": "GET",
-    })
-    .then(response => response.json()) 
-        .then(data => {    
-            console.log(data)
-            sorting(data);
-        })
-        .catch(err => {
-            console.log(err);
-        }); 
-
-    function sorting (data1) {
-        console.log(xyz);
-        data1.sort(function(a, b) {
-            return b.deaths - a.deaths;
-        });
+        if(sum4 % 2){
+            data1.sort(function(a, b) {
+                return b.deaths - a.deaths;
+            }); 
+        }else{
+            data1.sort(function(a, b) {
+                return a.deaths - b.deaths;
+            });
+        }
         
         var x = document.getElementById("stats");
         x.innerHTML = ``;
